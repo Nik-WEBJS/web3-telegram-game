80ebe551f2b0fd7b7b2368e16858f27841b73102
Nikita Fokin <nikita.fokin0123@gmail.com>
2025-02-18 23:22:54 +0500

    add updateBalance front eth

--- frontend/utils/UI.tsx
+++ frontend/utils/UI.tsx
@@ -1,12 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { BrowserProvider, formatEther } from "ethers";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 const UI = () => {
-  const [balance, setBalance] = useState(0);
+  const [balance, setBalance] = useState("0");
 
+  useEffect(() => {
+    const updateBalance = async () => {
+      if (!window.ethereum) return;
+      try {
+        const provider = new BrowserProvider(window.ethereum);
+
+        // Проверяем, есть ли уже подключенные аккаунты
+        const accounts = await provider.send("eth_accounts", []);
+        if (accounts.length === 0) {
+          // Если нет подключенных аккаунтов, запрашиваем разрешение
+          await provider.send("eth_requestAccounts", []);
+        }
+
+        const signer = await provider.getSigner();
+        const balance = await provider.getBalance(signer.address);
+        setBalance(formatEther(balance));
+      } catch (error) {
+        console.error("Ошибка получения баланса:", error);
+      }
+    };
+
+    updateBalance();
+  }, []);
   return (
     <div style={{ position: "absolute", top: 0, right: 0, padding: 10 }}>
       <p>Баланс: {balance} ETH</p>
-      <button onClick={() => alert("Войти в игру")}>Войти</button>
+      {!window.ethereum && <button onClick={() => alert("Войти в игру")}>Войти</button>}
     </div>
   );
 };
